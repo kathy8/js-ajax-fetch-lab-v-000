4002071e0113be9b2c75823930b5f6610c1e5dba
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-06-17 01:25:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+const baseURL = 'https://api.github.com'
+
 function getIssues() {
+  fetch(`${baseURL}/repos/jennianelson/javascript-fetch-lab/issues`, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showIssues(json))
+	 }
 }
 
 function showIssues(json) {
+  const issues = json.map(issue => {
+    return (`<h3>${issue.title}</h3>
+    <p>${issue.body}</p>`)
+  });
+  document.getElementById('issues').innerHTML = issues.join(' ');
 }
 
-function createIssue() {
+function createIssue() {const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
+  const postData = { title: title, body: body }
+  fetch(`${baseURL}/repos/jennianelson/javascript-fetch-lab/issues`, {
+    body: JSON.stringify(postData),
+    method: /post/,
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => getIssues(res))
+	 }
 }
 
 function showResults(json) {
+   document.getElementById('results').innerHTML = `<a href=${json.html_url}>Link to Repo</a>`;
 }
 
 function forkRepo() {
